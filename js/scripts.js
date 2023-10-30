a0a26c111cd99dbb67bc76a66a68a139479917b0
unknown <aymardrinel@outlook.fr>
2023-10-30 15:23:51 +0100

    Aymard : Commentaires des vins dont id est 10

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ const searchForm = document.getElementById('search-form'); // Le formulaire de r
 const searchInput = document.getElementById('search-input'); // Le champ de saisie de texte
 const wineList = document.getElementById('wine-list'); // Le conteneur pour les résultats
 
+
+
 // Écouteur d'événements pour le formulaire de recherche
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche la soumission du formulaire par défaut
@@ -53,4 +55,61 @@ function searchWines(Chateau) {
         .catch(error => {
             console.error('Une erreur s\'est produite :', error); // Gère les erreurs
         });
+}
+
+
+//COMMENTAIRE DES VINS DONT ID EST 10
+
+// Récupérer le bouton pour charger les commentaires et la section des commentaires
+const loadCommentsBtn = document.getElementById('load-comments-btn');
+const commentsSection = document.getElementById('comments-section');
+
+// Ajouter un écouteur d'événements pour le clic sur le bouton
+loadCommentsBtn.addEventListener('click', () => {
+    // Vérifier si la section des commentaires est déjà visible
+    if (commentsSection.style.display === 'none') {
+        findCommentsById('10'); // Charger les commentaires du vin ID 10
+        commentsSection.style.display = 'block'; // Afficher la section des commentaires
+    }
+});
+
+// Utilisation de la fonction pour récupérer les commentaires du vin ayant l'ID 10
+async function findCommentsById(id) {
+    if (id === '10') {
+        const apiURL10 = 'https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/10/comments';
+
+        try {
+            const response = await fetch(apiURL10);
+
+            if (response.ok) {
+                const comments = await response.json();
+                displayComments(comments); // Appel de la fonction pour afficher les commentaires
+            } else {
+                console.error('La requête a échoué avec le statut :', response.status);
+            }
+        } catch (error) {
+            console.error('Une erreur est survenue :', error);
+        }
+    }
+}
+
+// Fonction pour afficher les commentaires dans la section correspondante
+function displayComments(comments) {
+    const commentsList = document.getElementById('comments-list');
+
+    if (comments.length === 0) {
+        commentsList.textContent = "Aucun commentaire trouvé.";
+    } else {
+        comments.forEach(comment => {
+            const commentElement = document.createElement('div');
+            commentElement.classList.add('comment');
+            commentElement.innerHTML = `
+                <p>Auteur: ${comment.author}</p>
+                <p>Contenu: ${comment.content}</p>
+                <p>Date: ${comment.date}</p>
+                <hr>
+            `;
+            commentsList.appendChild(commentElement);
+        });
+    }
 }

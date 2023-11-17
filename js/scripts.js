3f9bd72f17254dbba2d55f16cbca20541eceaeea
unknown <aymardrinel@outlook.fr>
2023-11-17 18:41:49 +0100

    Recuperer commentaires vin 10 et Nombre de like vin 10

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 //Recherche les vins dont le nom contient ‘Chateau’
 // Récupération des éléments du DOM
 
-/*
-const searchForm = document.getElementById('search-form'); // Le formulaire de recherche
+
+/**const searchForm = document.getElementById('search-form'); // Le formulaire de recherche
 const searchInput = document.getElementById('search-input'); // Le champ de saisie de texte
 const wineList = document.getElementById('wine-list'); // Le conteneur pour les résultats
 
@@ -47,66 +47,80 @@ function searchWines(Chateau) {
         .catch(error => {
             console.error('Une erreur s\'est produite :', error); // Gère les erreurs
         });
-}
+}**/
 
 
-//COMMENTAIRE DES VINS DONT ID EST 10
 
-// Récupérer le bouton pour charger les commentaires et la section des commentaires
-const loadCommentsBtn = document.getElementById('load-comments-btn');
-const commentsSection = document.getElementById('comments-section');
+//RETROUVER LES COMMENTAIRE DU VIN  10 (aymard)
 
-// Ajouter un écouteur d'événements pour le clic sur le bouton
-loadCommentsBtn.addEventListener('click', () => {
-    // Vérifier si la section des commentaires est déjà visible
-    if (commentsSection.style.display === 'none') {
-        findCommentsById('10'); // Charger les commentaires du vin ID 10
-        commentsSection.style.display = 'block'; // Afficher la section des commentaires
-    }
-});
+async function findCommentsForWine(wine_id) {
+    const apiURL10 = `https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/${wine_id}/comments`;
 
-// Utilisation de la fonction pour récupérer les commentaires du vin ayant l'ID 10
-async function findCommentsById(id) {
-    if (id === '10') {
-        const apiURL10 = 'https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/10/comments';
-
-        try {
-            const response = await fetch(apiURL10);
-
-            if (response.ok) {
-                const comments = await response.json();
-                displayComments(comments); // Appel de la fonction pour afficher les commentaires
-            } else {
-                console.error('La requête a échoué avec le statut :', response.status);
-            }
-        } catch (error) {
-            console.error('Une erreur est survenue :', error);
-        }
-    }
-}
-
-// Fonction pour afficher les commentaires dans la section correspondante
-function displayComments(comments) {
-    const commentsList = document.getElementById('comments-list');
-
-    if (comments.length === 0) {
-        commentsList.textContent = "Aucun commentaire trouvé.";
-    } else {
-        comments.forEach(comment => {
-            const commentElement = document.createElement('div');
-            commentElement.classList.add('comment');
-            commentElement.innerHTML = `
-                <p>Auteur: ${comment.author}</p>
-                <p>Contenu: ${comment.content}</p>
-                <p>Date: ${comment.date}</p>
-                <hr>
-            `;
-            commentsList.appendChild(commentElement);
+    try {
+        const response = await fetch(apiURL10, {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+            },
         });
+        if (!response.ok) {
+            throw new Error(`La requête GET a échoué ${response.status}`)
+        } 
+        const comments = await response.json();
+        // Afficher le contenu de chaque commentaire
+        comments.forEach(comment => {
+            console.log('Commentaire:', comment.content);
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des commentaires :',error.message);
     }
 }
+//appel de la fonction
+findCommentsForWine(8);
 
-*/
+
+// RETROUVER LE NOMBRE DE LIKE DU VIN 10 (aymard)
+
+// Définition de la fonction asynchrone pour récupérer le nombre de likes pour le vin 10
+async function getLikeCount(wine_id) {
+    // Définition de l'URL de l'API
+    const apiURL11 = `https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines/${wine_id}/likes-count`;
+
+    try {
+        // Utilisation de l'API Fetch pour effectuer une requête GET
+        const response = await fetch(apiURL11, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // Vérification si la réponse est OK (statut HTTP 200-299)
+        if (!response.ok) {
+            // Si la réponse n'est pas OK, déclencher une erreur avec le statut HTTP
+            throw new Error(`La requête a échoué avec le statut ${response.status}`);
+        }
+
+        // Extraction des données JSON de la réponse
+        const data = await response.json();
+
+        // Affichage du nombre total de likes dans la console
+        console.log(`Nombre de likes pour le vin ${wine_id}  :`, data.total);
+    } catch (error) {
+        // Gestion des erreurs : affichage de l'erreur dans la console
+        console.error('Erreur lors de la récupération du nombre de likes :', error.message);
+    }
+}
+// Appel de la fonction asynchrone
+getLikeCount(7);
+
+
+//MODIFIER LE COMMENTAIRE 3 DU VIN 10 (aymard)
+
+
+  
+
+
 // Pour supprimer une image que l'utilisateur a ajouté d'un vin selectionné
 function deletePics(){
     const deletePics = document.forms['frmDELETE'].btAdd;              
